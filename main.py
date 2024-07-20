a10ad693d69742ddaa1a3737def2cb26cc490f14
vivek <chimnanivivek14@gmail.com>
2024-07-20 10:46:21 +0530

    update todo request

--- main.py
+++ main.py
@@ -49,3 +49,18 @@ async def create_todo(db: dependency_injection, todo_request: TodoRequest):
 
     db.add(todo_model)
     db.commit()
+
+
+@app.put("/todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def update_todo(db: dependency_injection, todo_request: TodoRequest, todo_id: int = Path(gt=0)):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    if todo_model is None:
+        raise HTTPException(status_code=404, detail="Todo not found!")
+
+    todo_model.title = todo_request.title
+    todo_model.description = todo_request.description
+    todo_model.priority = todo_request.priority
+    todo_model.completed = todo_request.completed
+
+    db.add(todo_model)
+    db.commit()
