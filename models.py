cb289b83f537f116ba115a114e5d8c2af15885b9
vivek <chimnanivivek14@gmail.com>
2024-07-23 07:00:37 +0530

    updated the database

--- models.py
+++ models.py
@@ -1,5 +1,18 @@
 from database import Base
-from sqlalchemy import Column, String, Boolean, Integer
+from sqlalchemy import Column, String, Boolean, Integer, ForeignKey
+
+
+class Users(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True)
+    email = Column(String, unique=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+    role = Column(String)
 
 
 class Todos(Base):
@@ -10,3 +23,4 @@ class Todos(Base):
     description = Column(String)
     priority = Column(Integer)
     completed = Column(Boolean, default=False)
+    owner = Column(Integer, ForeignKey("users.id"))
